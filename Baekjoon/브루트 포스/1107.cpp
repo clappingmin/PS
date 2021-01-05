0b78c8f4a02135ab3f9a9f3eaa61cd1417eda985
clappingmin <clappingmin@naver.com>
2021-01-06 02:07:50 +0900

    2021-01-05

--- Baekjoon/브루트 포스/1107.cpp
+++ Baekjoon/브루트 포스/1107.cpp
@@ -3,8 +3,64 @@
 
 using namespace std;
 
+bool broken[10];
+
+int possible(int c)
+{
+    if (c == 0) //이동할 채널이 0인데
+    {
+        if (broken[0]) //0버튼이 고장이 났으면
+            return 0;  //불가능
+        else
+            return 1;
+    }
+    int len = 0;
+    while (c > 0)
+    {
+        if (broken[c % 10])
+            return 0;
+        len += 1;
+        c /= 10;
+    }
+    return len;
+}
+
 int main()
 {
-    
+    int n, m, now = 100, res;
+
+    scanf("%d%d", &n, &m);
+
+    res = n - 100;
+
+    if (res < 0)
+        res = -res;
+
+    for (int i = 0; i < m; i++)
+    {
+        int tmp;
+        scanf("%d", &tmp);
+        broken[tmp] = true;
+    }
+
+    for (int i = 0; i <= 1000000; i++)
+    {
+        int c = i;
+        int len = possible(c);
+
+        if (len > 0) //버튼 이동이 가능하면
+        {
+            int press = c - n;
+
+            if (press < 0)
+                press = -press;
+
+            if (res > len + press)
+                res = len + press;
+        }
+    }
+
+    printf("%d", res);
+
     return 0;
 }
