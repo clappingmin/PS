679a9ee3d6195593a32aa60bcd89648190297f7a
clappingmin <clappingmin@naver.com>
2021-01-09 16:21:29 +0900

    2021-01-09

---
+++ Baekjoon/브루트 포스/그래프/6118(메모리초과).cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+#include <queue>
+#include <vector>
+#define MAX 20000 + 1
+
+using namespace std;
+
+int map[MAX][MAX];
+queue<int> q;
+int visited[MAX];
+int n, m;
+
+void bfs(int v)
+{
+    visited[v] = 1;
+    q.push(v);
+
+    while (!q.empty())
+    {
+        int tmp = q.front();
+        q.pop();
+
+        for (int i = 2; i <= n; i++)
+        {
+            if (visited[i] == 0 && map[tmp][i])
+            {
+                visited[i] = visited[tmp] + 1;
+                q.push(i);
+            }
+        }
+    }
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int num = 0, res = 0, cnt = 0;
+
+    cin >> n >> m;
+
+    for (int i = 0; i < m; i++)
+    {
+        int x, y;
+        cin >> x >> y;
+
+        map[x][y] = 1;
+        map[y][x] = 1;
+    }
+
+    bfs(1);
+
+    for (int i = 1; i <= n; i++)
+    {
+        if (visited[i] > res)
+        {
+            res = visited[i];
+            num = i;
+        }
+        else if (visited[i] == res)
+            cnt++;
+    }
+
+    cout << num << ' ' << res - 1 << ' ' << cnt << '\n';
+
+    return 0;
+}
