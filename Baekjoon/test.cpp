cf635797de50b976d1a710179034c737eb040a7a
clappingmin <clappingmin@naver.com>
2021-03-18 03:23:18 +0900

    2021-03-17

--- Baekjoon/test.cpp
+++ Baekjoon/test.cpp
@@ -1,84 +1,75 @@
 #include <iostream>
+#include <queue>
+#include <math.h>
 
 using namespace std;
 
-int n, m; //map 크기
-int map[51][51];
-
-pair<pair<int, int>, int> Robot; //로봇 초기 위치, 방향
+int N, L, R;
+int map[50][50];
+int visited[50][50];
+int th = 0;
+bool check = true;
 
 int dx[4] = {-1, 0, 1, 0};
 int dy[4] = {0, 1, 0, -1};
 
-int Turn_direction(int d)
+queue<pair<int, int>> pos; //연합국가 저장
+
+void BFS()
 {
-    if (d == 0)
-        return 3;
-    else if (d == 1)
-        return 0;
-    else if (d == 2)
-        return 1;
-    else if (d == 3)
-        return 2;
-}
-
-void Solution()
-{
-    int x = Robot.first.first;
-    int y = Robot.first.second;
-    int d = Robot.second;
-    int room = 0; //청소한 방의 개수
-
-    map[x][y] = 2; //시작지점 방 청소
-    room++;
-
-    int nx, ny, nd; //다음 청소 위치, 방향
-
-    while (1)
+    queue<pair<int, int>> q;
+    while (check)
     {
-        int tmp_d = d;
-        bool can_clean = false;
-        int cannotclean = 0;
+        check = false;
+        th++;
 
-        for (int i = 0; i < 4; i++)
+        for (int i = 0; i < N; i++)
         {
-            nd = Turn_direction(d);
-            nx = x + dx[nd];
-            ny = y + dy[nd];
-
-            if (map[nx][ny] == 0) //청소를 할 수 있는 경우
+            for (int j = 0; j < N; j++)
             {
-                can_clean = true;
-                break;
-            }
-            else if (map[nx][ny] == 1 || map[nx][ny] == 2 || nx < 0 || ny < 0 || nx >= n || ny >= m)
-            {
-                //벽이거나 이미 청소를 했거나 범위를 벗어난 경우
-                d = nd;
-                cannotclean++;
-            }
-        }
-        if (can_clean == true)
-        {
-            map[nx][ny] = 2;
-            room++;
-            d = nd;
-        }
-        if (cannotclean == 4)
-        {
-            nx = x - dx[tmp_d];
-            ny = y - dy[tmp_d]; //후진
-            d = tmp_d;
+                if (visited[i][j] == th)
+                    continue;
 
-            if (map[nx][ny] == 1 || nx < 0 || ny < 0 || nx >= n || ny >= m)
-            { //후진할 공간이 벽이거나 범위를 벗어난 경우
-                break;
+                int sum = map[i][j];
+                q.push({i, j});
+                pos.push({i, j});
+                visited[i][j] = th;
+
+                while (!q.empty())
+                {
+                    int x = q.front().first;
+                    int y = q.front().second;
+                    q.pop();
+
+                    for (int dir = 0; dir < 4; dir++)
+                    {
+                        int nx = x + dx[dir];
+                        int ny = y + dy[dir];
+
+                        if (nx < 0 || ny < 0 || nx >= N || ny >= N)
+                            continue;
+
+                        if (visited[nx][ny] == th || abs(map[x][y] - map[nx][ny]) < L || abs(map[x][y] - map[nx][ny]) > R)
+                            continue;
+
+                        q.push({nx, ny});
+                        pos.push({nx, ny});
+                        sum += map[nx][ny];
+                        visited[nx][ny] = th;
+                        check = true;
+                    }
+                }
+
+                int avr = sum / pos.size();
+
+                while (!pos.empty())
+                {
+                    map[pos.front().first][pos.front().second] = avr;
+                    pos.pop();
+                }
             }
         }
-        x = nx;
-        y = ny;
     }
-    cout << room << '\n';
 }
 
 int main()
@@ -86,14 +77,15 @@ int main()
     ios::sync_with_stdio(false);
     cin.tie(0);
 
-    cin >> n >> m;
-    cin >> Robot.first.first >> Robot.first.second >> Robot.second;
+    cin >> N >> L >> R;
 
-    for (int i = 0; i < n; i++)
-        for (int j = 0; j < m; j++)
+    for (int i = 0; i < N; i++)
+        for (int j = 0; j < N; j++)
             cin >> map[i][j];
 
-    Solution();
+    BFS();
+
+    cout << th - 1;
 
     return 0;
 }
--- Baekjoon/브루트 포스/시뮬레이션/16234.cpp
+++ Baekjoon/브루트 포스/시뮬레이션/16234.cpp
@@ -1,35 +1,39 @@
 #include <iostream>
 #include <queue>
 #include <math.h>
+
 using namespace std;
 
-int n, l, r;
+int N, L, R;
 int map[50][50];
 int visited[50][50];
+int th = 0;
+bool check = true;
+
 int dx[4] = {-1, 0, 1, 0};
 int dy[4] = {0, 1, 0, -1};
-queue<pair<int, int>> q;   //bfs용
-queue<pair<int, int>> pos; //연합국가 저장용
-bool check = true;
-int cnt = 0;
 
-void bfs()
+queue<pair<int, int>> pos; //연합국가 저장
+
+void BFS()
 {
+    queue<pair<int, int>> q;
     while (check)
     {
-        cnt++;
         check = false;
+        th++;
 
-        for (int i = 0; i < n; i++)
+        for (int i = 0; i < N; i++)
         {
-            for (int j = 0; j < n; j++)
+            for (int j = 0; j < N; j++)
             {
-                if (visited[i][j] == cnt)
+                if (visited[i][j] == th)
                     continue;
+
                 int sum = map[i][j];
                 q.push({i, j});
                 pos.push({i, j});
-                visited[i][j] = cnt;
+                visited[i][j] = th;
 
                 while (!q.empty())
                 {
@@ -42,17 +46,17 @@ void bfs()
                         int nx = x + dx[dir];
                         int ny = y + dy[dir];
 
-                        if (nx < 0 || nx >= n || ny < 0 || ny >= n)
+                        if (nx < 0 || ny < 0 || nx >= N || ny >= N)
                             continue;
 
-                        if (visited[nx][ny] == cnt || abs(map[x][y] - map[nx][ny]) < l || abs(map[x][y] - map[nx][ny]) > r)
+                        if (visited[nx][ny] == th || abs(map[x][y] - map[nx][ny]) < L || abs(map[x][y] - map[nx][ny]) > R)
                             continue;
 
-                        check = true;
                         q.push({nx, ny});
                         pos.push({nx, ny});
                         sum += map[nx][ny];
-                        visited[nx][ny] = cnt;
+                        visited[nx][ny] = th;
+                        check = true;
                     }
                 }
 
@@ -70,15 +74,18 @@ void bfs()
 
 int main()
 {
-    cin >> n >> l >> r;
+    ios::sync_with_stdio(false);
+    cin.tie(0);
 
-    for (int i = 0; i < n; i++)
-        for (int j = 0; j < n; j++)
+    cin >> N >> L >> R;
+
+    for (int i = 0; i < N; i++)
+        for (int j = 0; j < N; j++)
             cin >> map[i][j];
 
-    bfs();
+    BFS();
 
-    cout << cnt - 1 << '\n';
+    cout << th - 1;
 
     return 0;
 }
