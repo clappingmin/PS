651df822bc6b9298dcd8245e7c5a3372d25d0bcd
clappingmin <clappingmin@naver.com>
2021-03-19 00:11:45 +0900

    2021-03-18

--- Baekjoon/test.cpp
+++ Baekjoon/test.cpp
@@ -1,119 +1,228 @@
 #include <iostream>
 #include <vector>
-#include <queue>
 #include <cmath>
-#include <cstring>
 
 using namespace std;
 
 int N, M;
-int time, res = 987654321;
-int map[50][50], tmp[50][50];
-int check_v[2501];
+int map[8][8], tmp_map[8][8];
+int empty_cnt = 987654321;
+int cctv_cnt;
 
-vector<pair<int, int>> virus, v;
+vector<pair<pair<int, int>, pair<int, int>>> cctv; //좌표, 번호, 방향
 
-int dx[4] = {-1, 0, 1, 0};
-int dy[4] = {0, 1, 0, -1};
-
-void Spread_virus()
-{
-    queue<pair<int, int>> q;
-    time = 0;
-
-    for (int i = 0; i < v.size(); i++)
-    {
-        tmp[v[i].first][v[i].second] = 0;
-        q.push(v[i]);
-    }
-
-    while (!q.empty())
-    {
-        int x = q.front().first;
-        int y = q.front().second;
-        q.pop();
-
-        for (int dir = 0; dir < 4; dir++)
-        {
-            int nx = x + dx[dir];
-            int ny = y + dy[dir];
-
-            if (nx < 0 || ny < 0 || nx >= N || ny >= N)
-                continue;
-            if (map[nx][ny] == 1 || tmp[nx][ny] != -1)
-                //벽이거나 이미 방문 했으면
-                continue;
-
-            tmp[nx][ny] = tmp[x][y] + 1;
-            q.push({nx, ny});
-
-            if (time < tmp[nx][ny])
-                time = tmp[nx][ny];
-        }
-    }
-}
-
-bool Check()
+void Copy_map()
 {
     for (int i = 0; i < N; i++)
-    {
-        for (int j = 0; j < N; j++)
-        {
-            if (map[i][j] != 1 && tmp[i][j] == -1)
-                return false;
-        }
-    }
-    return true;
+        for (int j = 0; j < M; j++)
+            tmp_map[i][j] = map[i][j];
 }
 
-void Select(int index, int cnt)
+void up(int r, int c)
 {
-    if (cnt == M)
+    for (int i = r - 1; i >= 0; i--)
     {
-        memset(tmp, -1, sizeof(tmp));
-        Spread_virus();
+        if (tmp_map[i][c] == 6)
+            break;
 
-        if (Check())
+        else if (1 <= tmp_map[i][c] && tmp_map[i][c] <= 5)
+            continue;
+
+        tmp_map[i][c] = -1;
+    }
+}
+void down(int r, int c)
+{
+    for (int i = r + 1; i < N; i++)
+    {
+        if (tmp_map[i][c] == 6)
+            break;
+
+        else if (1 <= tmp_map[i][c] && tmp_map[i][c] <= 5)
+            continue;
+
+        tmp_map[i][c] = -1;
+    }
+}
+void left(int r, int c)
+{
+    for (int i = c - 1; i >= 0; i--)
+    {
+        if (tmp_map[r][i] == 6)
+            break;
+        else if (1 <= tmp_map[r][i] && tmp_map[r][i] <= 5)
+            continue;
+
+        tmp_map[r][i] = -1;
+    }
+}
+void right(int r, int c)
+{
+    for (int i = c + 1; i < M; i++)
+    {
+        if (tmp_map[r][i] == 6)
+            break;
+        else if (1 <= tmp_map[r][i] && tmp_map[r][i] <= 5)
+            continue;
+
+        tmp_map[r][i] = -1;
+    }
+}
+
+int Count_empty()
+{
+    int res = 0;
+    for (int i = 0; i < N; i++)
+    {
+        for (int j = 0; j < M; j++)
         {
-            res = min(res, time);
+            if (tmp_map[i][j] == 0)
+                res += 1;
         }
+    }
+    return res;
+}
 
+void Setting_cctv()
+{
+    Copy_map();
+
+    for (int i = 0; i < cctv_cnt; i++)
+    {
+        int cctv_num = cctv[i].second.first;
+        int cctv_dir = cctv[i].second.second;
+        int x = cctv[i].first.first;
+        int y = cctv[i].first.second;
+
+        if (cctv_num == 1)
+        {
+            if (cctv_dir == 0)
+                up(x, y);
+            else if (cctv_dir == 1)
+                down(x, y);
+            else if (cctv_dir == 2)
+                left(x, y);
+            else if (cctv_dir == 3)
+                right(x, y);
+        }
+        else if (cctv_num == 2)
+        {
+            if (cctv_dir == 0 || cctv_dir == 1)
+            {
+                up(x, y);
+                down(x, y);
+            }
+            else if (cctv_dir == 2 || cctv_dir == 3)
+            {
+                left(x, y);
+                right(x, y);
+            }
+        }
+        else if (cctv_num == 3)
+        {
+            if (cctv_dir == 0)
+            {
+                up(x, y);
+                left(x, y);
+            }
+            else if (cctv_dir == 1)
+            {
+                up(x, y);
+                right(x, y);
+            }
+            else if (cctv_dir == 2)
+            {
+                down(x, y);
+                left(x, y);
+            }
+            else if (cctv_dir == 3)
+            {
+                down(x, y);
+                right(x, y);
+            }
+        }
+        else if (cctv_num == 4)
+        {
+            if (cctv_dir == 0)
+            {
+                up(x, y);
+                left(x, y);
+                right(x, y);
+            }
+            else if (cctv_dir == 1)
+            {
+                up(x, y);
+                left(x, y);
+                down(x, y);
+            }
+            else if (cctv_dir == 2)
+            {
+                down(x, y);
+                left(x, y);
+                right(x, y);
+            }
+            else if (cctv_dir == 3)
+            {
+                up(x, y);
+                down(x, y);
+                right(x, y);
+            }
+        }
+        else if (cctv_num == 5)
+        {
+
+            up(x, y);
+            down(x, y);
+            left(x, y);
+            right(x, y);
+        }
+    }
+}
+
+void Select_direction(int cnt)
+{
+    if (cnt == cctv_cnt)
+    {
+        Setting_cctv();
+        empty_cnt = min(empty_cnt, Count_empty());
         return;
     }
 
-    for (int i = index; i < virus.size(); i++)
-    {
-        if (check_v[i] == true)
-            continue;
+    cctv[cnt].second.second = 0;
+    Select_direction(cnt + 1);
 
-        check_v[i] = true;
-        v.push_back(virus[i]);
+    cctv[cnt].second.second = 1;
+    Select_direction(cnt + 1);
 
-        Select(i, cnt + 1);
+    cctv[cnt].second.second = 2;
+    Select_direction(cnt + 1);
 
-        check_v[i] = false;
-        v.pop_back();
-    }
+    cctv[cnt].second.second = 3;
+    Select_direction(cnt + 1);
 }
 
 int main()
 {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
     cin >> N >> M;
 
     for (int i = 0; i < N; i++)
     {
-        for (int j = 0; j < N; j++)
+        for (int j = 0; j < M; j++)
         {
             cin >> map[i][j];
-            if (map[i][j] == 2)
-                virus.push_back({i, j});
+
+            if (1 <= map[i][j] && map[i][j] <= 5)
+                cctv.push_back({{i, j}, {map[i][j], -1}});
         }
     }
+    cctv_cnt = cctv.size();
 
-    Select(0, 0);
+    Select_direction(0);
 
-    if (res == 987654321)
-        cout << -1 << '\n';
-    else
-        cout << res << '\n';
+    cout << empty_cnt << '\n';
+
+    return 0;
 }
