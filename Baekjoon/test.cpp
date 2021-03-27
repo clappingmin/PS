078a94dd41acbd125cca7e67418701f049b948f2
clappingmin <clappingmin@naver.com>
2021-03-28 04:51:49 +0900

    2021-03-27

--- Baekjoon/test.cpp
+++ Baekjoon/test.cpp
@@ -1,82 +1,111 @@
 #include <iostream>
-#include <vector>
-#include <cmath>
+#include <queue>
 
 using namespace std;
 
-int N, M;
-int map[50][50];
-int res = 987654321;
+int N, M, X, Y, K; //지도 크기, 주사위 놓을 좌표, 명령 개수
+int map[20][20];
 
-vector<pair<int, int>> chicken, house, v;
-bool check[13];
+int dx[] = {0, 0, 0, -1, 1};
+int dy[] = {0, 1, -1, 0, 0};
+queue<int> cmd;
+int dice[7];
 
-int Solution()
+void RollDice(int c)
 {
-    int sum = 0;
-    for (int i = 0; i < house.size(); i++)
+    int d1 = dice[1];
+    int d2 = dice[2];
+    int d3 = dice[3];
+    int d4 = dice[4];
+    int d5 = dice[5];
+
+    int d6 = dice[6];
+    if (c == 1) //동쪽으로
     {
-        int x1 = house[i].first;
-        int y1 = house[i].second;
-        int d = 987654321;
-
-        for (int j = 0; j < v.size(); j++)
-        {
-            int x2 = v[j].first;
-            int y2 = v[j].second;
-            int dist = abs(x2 - x1) + abs(y2 - y1);
-
-            d = min(d, dist);
-        }
-        sum += d;
+        dice[1] = d4;
+        dice[3] = d1;
+        dice[6] = d3;
+        dice[4] = d6;
     }
-    return sum;
+    else if (c == 2) //서쪽으로
+    {
+        dice[1] = d3;
+        dice[4] = d1;
+        dice[6] = d4;
+        dice[3] = d6;
+    }
+    else if (c == 3) //북쪽으로
+    {
+        dice[1] = d5;
+        dice[5] = d6;
+        dice[6] = d2;
+        dice[2] = d1;
+    }
+    else if (c == 4) //남쪽으로
+    {
+        dice[1] = d2;
+        dice[5] = d1;
+        dice[6] = d5;
+        dice[2] = d6;
+    }
 }
 
-void Select(int index, int cnt)
+void Solution()
 {
-    if (cnt == M)
+    int x = X;
+    int y = Y;
+    while (!cmd.empty())
     {
-        res = min(res, Solution());
-        return;
-    }
-    for (int i = index; i < chicken.size(); i++)
-    {
-        if (check[i] == true)
+        int command = cmd.front();
+        cmd.pop();
+
+        int nx = x + dx[command];
+        int ny = y + dy[command];
+
+        if (nx < 0 || nx >= N || ny < 0 || ny >= M)
             continue;
 
-        check[i] = true;
-        v.push_back(chicken[i]);
+        RollDice(command);
 
-        Select(i, cnt + 1);
+        if (map[nx][ny] != 0)
+        {
+            dice[6] = map[nx][ny];
+            map[nx][ny] = 0;
+        }
+        else if (map[nx][ny] == 0)
+        {
+            map[nx][ny] = dice[6];
+        }
 
-        check[i] = false;
-        v.pop_back();
+        cout << dice[1] << '\n';
+
+        x = nx;
+        y = ny;
     }
 }
-
 int main()
 {
     ios::sync_with_stdio(false);
     cin.tie(0);
 
-    cin >> N >> M;
+    cin >> N >> M >> X >> Y >> K;
 
     for (int i = 0; i < N; i++)
     {
-        for (int j = 0; j < N; j++)
+        for (int j = 0; j < M; j++)
         {
             cin >> map[i][j];
-
-            if (map[i][j] == 1)
-                house.push_back({i, j});
-            else if (map[i][j] == 2)
-                chicken.push_back({i, j});
         }
     }
 
-    Select(0, 0);
-    cout << res << '\n';
+    for (int i = 0; i < K; i++)
+    {
+        int tmp;
+        cin >> tmp;
+        cmd.push(tmp);
+    }
+
+    Solution();
 
     return 0;
 }
