cfd5276b623733a5510f86c0fb214bf7dfe55658
clappingmin <clappingmin@naver.com>
2021-03-30 00:45:02 +0900

    2021-03-29

--- Baekjoon/test.cpp
+++ Baekjoon/test.cpp
@@ -1,69 +1,46 @@
 #include <iostream>
+#include <string>
+#include <vector>
 
 using namespace std;
 
-int N, M;
-char map[50][50];
-bool visited[50][50];
-int dist[50][50];
-int dx[] = {-1, 0, 1, 0};
-int dy[] = {0, 1, 0, -1};
-
-bool DFS(int x, int y, int cnt)
+string solution(string new_id)
 {
-    if (visited[x][y])
+    string answer = "";
+
+    string temp = "";
+    for (int i = 0; i < new_id.length(); i++)
     {
-        if (cnt - dist[x][y] >= 4)
-            return true;
-        else
-            return false;
+        char C = new_id[i];
+        
+        if ('A' <= C && C <= 'Z') //1단계
+            temp += tolower(C);
+        else if (('a' <= C && C <= 'z') || ('0' <= C && C <= '9') || C == '-' || C == '_' || C == '.')//2단계
+            temp += C;
     }
-    visited[x][y] = true;
-    dist[x][y] = cnt;
 
-    for (int dir = 0; dir < 4; dir++)
+    string temp2 ="";
+    temp2 += temp[0];
+    for (int i = 1; i < temp.length(); i++) //3단계
     {
-        int nx = x + dx[dir];
-        int ny = y + dy[dir];
-
-        if (nx < 0 || ny < 0 || nx >= N || ny >= M)
+        if (temp[i] == '.' && temp[i] == temp[i - 1])
             continue;
-
-        if (map[nx][ny] != map[x][y])
-            continue;
-
-        if (DFS(nx, ny, cnt + 1))
-            return true;
+        temp2 += temp[i];
     }
-    return false;
+
+    if (Str[0] == '.') Str = Str.substr(1);
+    if (Str.length() >= 1 && Str[Str.length() - 1] == '.') Str = Str.substr(0, Str.length() - 1);
+
+
+
+    return temp2;
 }
 
 int main()
 {
-    ios::sync_with_stdio(false);
-    cin.tie(0);
+    string s = "...!@BaT#*..y.abcdefghijklm";
 
-    cin >> N >> M;
-
-    for (int i = 0; i < N; i++)
-        for (int j = 0; j < M; j++)
-            cin >> map[i][j];
-
-    string res = "No";
-
-    for (int i = 0; i < N; i++)
-    {
-        for (int j = 0; j < M; j++)
-        {
-            if (!visited[i][j])
-            {
-                if (DFS(i, j, 0))
-                    res = "Yes";
-            }
-        }
-    }
-
-    cout << res << "\n";
+    cout<< solution(s)<<'\n';
 
     return 0;
 }
