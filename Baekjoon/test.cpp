eec195de79e8495caf8cfad98ab1e766c2f63030
clappingmin <clappingmin@naver.com>
2021-06-01 22:53:48 +0900

    2021-06-01

--- Baekjoon/test.cpp
+++ Baekjoon/test.cpp
@@ -1,85 +1,36 @@
 #include <iostream>
-#include <string>
-#include <cstring>
+#include <algorithm>
 
 using namespace std;
 
-char map[50][50];
-int dist[50][50];
-bool check[50][50];
-int n, m;
-int dx[] = {-1, 0, 1, 0};
-int dy[] = {0, 1, 0, -1};
-
-bool DFS(int x, int y, int cnt)
-{
-	if (check[x][y])
-	{
-		if (cnt - dist[x][y] >= 4)
-			return true;
-		else
-			return false;
-	}
-	dist[x][y] = cnt;
-	check[x][y] = true;
-
-	for (int dir = 0; dir < 4; dir++)
-	{
-		int nx = x + dx[dir];
-		int ny = y + dy[dir];
-
-		if (nx < 0 || ny < 0 || nx >= n || ny >= m)
-			continue;
-
-		if (map[nx][ny] != map[x][y])
-			continue;
-
-		if (DFS(nx, ny, cnt + 1))
-			return true;
-	}
-	return false;
-}
+int n, k; //물품의 수, 준서가 버틸 수 있는 무게
+int w[101], v[101];
+int dp[101][100001];
 
 int main()
 {
 	ios::sync_with_stdio(false);
 	cin.tie(0);
 
-	cin >> n >> m;
+	cin >> n >> k;
 
-	for (int i = 0; i < n; i++)
+	for (int i = 1; i <= n; i++)
 	{
-		for (int j = 0; j < m; j++)
+		cin >> w[i] >> v[i];
+	}
+
+	for (int i = 1; i <= n; i++)
+	{
+		for (int j = 1; j <= k; j++)
 		{
-			cin >> map[i][j];
+			if (j >= w[i])
+				dp[i][j] = max(dp[i - 1][j], dp[i - 1][j - w[i]] + v[i]);
+			else
+				dp[i][j] = dp[i - 1][j]; //담지 않을 경우
 		}
 	}
 
-	string res = "No";
+	cout << dp[n][k] << '\n';
 
-	for (int i = 0; i < n; i++)
-	{
-		for (int j = 0; j < m; j++)
-		{
-			if (!check[i][j])
-			{
-				if (DFS(i, j, 0))
-				{
-					res = "Yes";
-					break;
-				}
-			}
-		}
-	}
-
-	for (int i = 0; i < n; i++)
-	{
-		for (int j = 0; j < m; j++)
-		{
-			cout << dist[i][j];
-		}
-		cout << '\n';
-	}
-	cout << res << '\n';
 	return 0;
 }
