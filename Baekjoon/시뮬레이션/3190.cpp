eb44b306afa611dfc8e25f7e463edfe66dea4a08
clappingmin <clappingmin@naver.com>
2021-06-02 21:35:10 +0900

    2021-06-02

--- Baekjoon/시뮬레이션/3190.cpp
+++ Baekjoon/시뮬레이션/3190.cpp
@@ -1,19 +1,18 @@
 #include <iostream>
-#include <deque>
 #include <queue>
+#include <deque>
 
 using namespace std;
 
-int N, K, L;
-int map[101][101];
+int n, k, l; //보드 크기, 사과 개수, 뱀 변환 횟수
+int map[100][100];
 queue<pair<int, char>> cmd;
-bool check = true;
 int dx[] = {0, 1, 0, -1};
 int dy[] = {1, 0, -1, 0};
 
 int Turn_snake(int d, char c)
 {
-	if (c == 'D') //오른쪽일 경우
+	if (c == 'D') //오른쪽
 	{
 		if (d == 0)
 			return 1;
@@ -24,7 +23,7 @@ int Turn_snake(int d, char c)
 		else if (d == 3)
 			return 0;
 	}
-	else if (c == 'L') //왼쪽일 경우
+	else if (c == 'L')
 	{
 		if (d == 0)
 			return 3;
@@ -40,73 +39,73 @@ int Turn_snake(int d, char c)
 void Solution()
 {
 	deque<pair<int, int>> snake;
-	int d = 0;
 	snake.push_back({0, 0});
-	map[0][0] = 2; //뱀 표시
+	int d = 0; //시작 뱀 방향: 오른쪽
 	int time = 0;
-
-	int cmd_time = cmd.front().first;
-	char cmd_dir = cmd.front().second;
-	cmd.pop();
+	map[0][0] = 2;
 
 	while (true)
 	{
 		time++;
 
-		int nx = snake.front().first + dx[d];
-		int ny = snake.front().second + dy[d];
+		int x = snake.front().first;
+		int y = snake.front().second;
 
-		if (nx < 0 || ny < 0 || nx >= N || ny >= N || map[nx][ny] == 2)
+		int nx = x + dx[d];
+		int ny = y + dy[d];
+
+		if (nx < 0 || ny < 0 || nx >= n || ny >= n || map[nx][ny] == 2)
 			break;
 
-		if (map[nx][ny] == 0)
+		if (map[nx][ny] == 1) //사과일 경우
+		{
+			snake.push_front({nx, ny});
+			map[nx][ny] = 2;
+		}
+		else if (map[nx][ny] == 0) //빈공간 일 경우
 		{
 			map[nx][ny] = 2;
+			snake.push_front({nx, ny});
 			map[snake.back().first][snake.back().second] = 0;
 			snake.pop_back();
-			snake.push_front({nx, ny});
 		}
 
-		else if (map[nx][ny] == 1)
+		if (!cmd.empty())
 		{
-			map[nx][ny] = 2;
-			snake.push_front({nx, ny});
-		}
-		
-		if (time == cmd_time)
-		{
-			d = Turn_snake(d, cmd_dir);
-			if (!cmd.empty())
+			if (time == cmd.front().first)
 			{
-				cmd_time = cmd.front().first;
-				cmd_dir = cmd.front().second;
+				d = Turn_snake(d, cmd.front().second);
 				cmd.pop();
 			}
 		}
 	}
 	cout << time << '\n';
 }
+
 int main()
 {
 	ios::sync_with_stdio(false);
 	cin.tie(0);
 
-	cin >> N >> K;
+	cin >> n >> k;
 
-	for (int i = 0; i < K; i++)
+	for (int i = 0; i < k; i++)
 	{
 		int x, y;
 		cin >> x >> y;
-		map[x - 1][y - 1] = 1; //사과
+
+		map[x - 1][y - 1] = 1; //사과 좌표
 	}
 
-	cin >> L;
+	cin >> l;
 
-	for (int i = 0; i < L; i++)
+	for (int i = 0; i < l; i++)
 	{
 		int x;
 		char c;
+
 		cin >> x >> c;
+
 		cmd.push({x, c});
 	}
 	Solution();
