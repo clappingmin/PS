895640cbe3a29cf4e9eb5a57310d757cb172eec2
clappingmin <clappingmin@naver.com>
2021-06-03 23:06:15 +0900

    2021-06-03

--- Baekjoon/test.cpp
+++ Baekjoon/test.cpp
@@ -1,73 +1,113 @@
 #include <iostream>
+#include <vector>
+#include <algorithm>
 #include <queue>
-#include <set>
-#include <string>
-
-#define ll long long
+#include <cstring>
 
 using namespace std;
 
-char op[] = {'*', '+', '-', '/'};
-ll s, t;
+int n, m;
+int map[50][50];
+int check_map[50][50];
+vector<pair<int, int>> virus;
+bool check_virus[10];
+int res = 987654321;
+int dx[] = {-1, 0, 1, 0};
+int dy[] = {0, 1, 0, -1};
+int empty_cnt = 0;
 
-ll Calculation(ll x, char c)
+int count_virus()
 {
-	if (c == '*')
-		return (x * x);
-	else if (c == '+')
-		return (x + x);
-	else if (c == '-')
-		return (x - x);
-	else if (c == '/')
-		return (x / x);
-}
+	queue<pair<int, int>> q;
+	int time = 0;
+	int cnt = 0;
+	memset(check_map, -1, sizeof(check_map));
 
-void BFS()
-{
-	queue<pair<ll, string>> q;
-	set<ll> se;
-	q.push({s, ""});
-	se.insert(s);
+	for (int i = 0; i < 10; i++)
+	{
+		if (check_virus[i])
+		{
+			q.push(virus[i]);
+			check_map[virus[i].first][virus[i].second] = 0;
+		}
+	}
 
 	while (!q.empty())
 	{
-		ll x = q.front().first;
-		string str = q.front().second;
+		int x = q.front().first;
+		int y = q.front().second;
 		q.pop();
 
-		if (x == t)
-		{
-			cout << str << '\n';
-			return;
-		}
 		for (int dir = 0; dir < 4; dir++)
 		{
-			ll nx = Calculation(x, op[dir]);
+			int nx = x + dx[dir];
+			int ny = y + dy[dir];
 
-			if (nx < 1)
+			if (nx < 0 || ny < 0 || nx >= n || ny >= n)
 				continue;
 
-			if (se.find(nx) != se.end()) //이미 있을 경우
+			if (map[nx][ny] == 1 || check_map[nx][ny] != -1)
 				continue;
 
-			se.insert(nx);
-			q.push({nx, str + op[dir]});
+			check_map[nx][ny] = check_map[x][y] + 1;
+			q.push({nx, ny});
+
+			if (map[nx][ny] == 0)
+			{
+				cnt++;
+				time = check_map[nx][ny];
+			}
 		}
+
+		if (empty_cnt == cnt)
+			return time;
 	}
-	cout << "-1\n";
+	return 987654321;
 }
 
+void choice_virus(int idx, int cnt)
+{
+	if (cnt == m)
+	{
+		res = min(res, count_virus());
+	}
+	for (int i = idx; i < virus.size(); i++)
+	{
+		if (check_virus[i])
+			continue;
+
+		check_virus[i] = true;
+		choice_virus(i, cnt + 1);
+		check_virus[i] = false;
+	}
+}
 int main()
 {
 	ios::sync_with_stdio(false);
 	cin.tie(0);
 
-	cin >> s >> t;
+	cin >> n >> m;
 
-	if (s == t)
-		cout << 0 << '\n';
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < n; j++)
+		{
+			cin >> map[i][j];
+
+			if (map[i][j] == 2)
+				virus.push_back({i, j});
+
+			else if (map[i][j] == 0)
+				empty_cnt++;
+		}
+	}
+
+	choice_virus(0, 0); //1.활성할 바이러스 선택
+
+	if (res == 987654321)
+		cout << -1 << '\n';
 	else
-		BFS();
+		cout << res << '\n';
 
 	return 0;
 }
--- Baekjoon/브루트 포스/17142.cpp
+++ Baekjoon/브루트 포스/17142.cpp
@@ -1,116 +1,113 @@
 #include <iostream>
-#include <queue>
 #include <vector>
-#include <cstring>
 #include <algorithm>
+#include <queue>
+#include <cstring>
 
 using namespace std;
 
-int N, M;
+int n, m;
 int map[50][50];
-int check[50][50];
+int check_map[50][50];
 vector<pair<int, int>> virus;
-vector<int> active_virus;
-bool check_virus[251];
-int empty_place = 0;
-int time = 987654321;
-
+bool check_virus[10];
+int res = 987654321;
 int dx[] = {-1, 0, 1, 0};
 int dy[] = {0, 1, 0, -1};
+int empty_cnt = 0;
 
-void Spread_virus()
+int count_virus()
 {
-    memset(check, -1, sizeof(check));
+	queue<pair<int, int>> q;
+	int time = 0;
+	int cnt = 0;
+	memset(check_map, -1, sizeof(check_map));
 
-    queue<pair<int, int>> q;
+	for (int i = 0; i < 10; i++)
+	{
+		if (check_virus[i])
+		{
+			q.push(virus[i]);
+			check_map[virus[i].first][virus[i].second] = 0;
+		}
+	}
 
-    for (int i = 0; i < M; i++)
-    {
-        int x = virus[active_virus[i]].first;
-        int y = virus[active_virus[i]].second;
-        q.push({x, y});
-        check[x][y] = 0;
-    }
-    int total_time = 0;
-    int place_cnt = 0;
+	while (!q.empty())
+	{
+		int x = q.front().first;
+		int y = q.front().second;
+		q.pop();
 
-    while (!q.empty())
-    {
-        int x = q.front().first;
-        int y = q.front().second;
-        q.pop();
+		for (int dir = 0; dir < 4; dir++)
+		{
+			int nx = x + dx[dir];
+			int ny = y + dy[dir];
 
-        for (int dir = 0; dir < 4; dir++)
-        {
-            int nx = x + dx[dir];
-            int ny = y + dy[dir];
+			if (nx < 0 || ny < 0 || nx >= n || ny >= n)
+				continue;
 
-            if (nx < 0 || ny < 0 || nx >= N || ny >= N)
-                continue;
+			if (map[nx][ny] == 1 || check_map[nx][ny] != -1)
+				continue;
 
-            if (map[nx][ny] == 1 || check[nx][ny] != -1)
-                continue;
+			check_map[nx][ny] = check_map[x][y] + 1;
+			q.push({nx, ny});
 
-            check[nx][ny] = check[x][y] + 1;
-            q.push({nx, ny});
+			if (map[nx][ny] == 0)
+			{
+				cnt++;
+				time = check_map[nx][ny];
+			}
+		}
 
-            if (map[nx][ny] == 0)
-            {
-                place_cnt++;
-                total_time = check[nx][ny];
-            }
-        }
-    }
-
-    if (empty_place == place_cnt)
-        time = min(time, total_time);
+		if (empty_cnt == cnt)
+			return time;
+	}
+	return 987654321;
 }
 
-void Choice_virus(int idx, int cnt)
+void choice_virus(int idx, int cnt)
 {
-    if (cnt == M)
-    {
-        Spread_virus(); //2.바이러스 퍼뜨리기
-        return;
-    }
-    for (int i = idx; i < virus.size(); i++)
-    {
-        if (check_virus[i])
-            continue;
+	if (cnt == m)
+	{
+		res = min(res, count_virus());
+	}
+	for (int i = idx; i < virus.size(); i++)
+	{
+		if (check_virus[i])
+			continue;
 
-        check_virus[i] = true;
-        active_virus.push_back(i);
-        Choice_virus(i, cnt + 1);
-        active_virus.pop_back();
-        check_virus[i] = false;
-    }
+		check_virus[i] = true;
+		choice_virus(i, cnt + 1);
+		check_virus[i] = false;
+	}
 }
-
 int main()
 {
-    ios::sync_with_stdio(false);
-    cin.tie(0);
+	ios::sync_with_stdio(false);
+	cin.tie(0);
 
-    cin >> N >> M;
+	cin >> n >> m;
 
-    for (int i = 0; i < N; i++)
-    {
-        for (int j = 0; j < N; j++)
-        {
-            cin >> map[i][j];
-            if (map[i][j] == 2) //바이러스일 경우
-                virus.push_back({i, j});
-            else if (map[i][j] == 0)
-                empty_place++;
-        }
-    }
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < n; j++)
+		{
+			cin >> map[i][j];
 
-    Choice_virus(0, 0); //1.활성 바이러스 선택하기
+			if (map[i][j] == 2)
+				virus.push_back({i, j});
 
-    if (time == 987654321)
-        cout << -1 << '\n';
-    else
-        cout << time << '\n';
+			else if (map[i][j] == 0)
+				empty_cnt++;
+		}
+	}
 
-    return 0;
+	choice_virus(0, 0); //1.활성할 바이러스 선택
+
+	if (res == 987654321)
+		cout << -1 << '\n';
+	else
+		cout << res << '\n';
+
+	return 0;
 }
