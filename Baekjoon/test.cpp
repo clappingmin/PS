114150a253c0df091dfc454f0971d0365a2402d2
clappingmin <clappingmin@naver.com>
2021-06-07 01:13:17 +0900

    2021-06-06

--- Baekjoon/test.cpp
+++ Baekjoon/test.cpp
@@ -1,70 +1,130 @@
 #include <iostream>
 #include <queue>
-#include <string>
-#include <set>
-#define ll long long
 
 using namespace std;
 
-char op[] = {'+', '-', '*', '/'};
-ll s, t;
+int r, c;
+char map[1500][1500];
+bool check[1500][1500];
+queue<pair<int, int>> water;
+queue<pair<int,int>> water_ice;
+queue<pair<int,int>> swan;
+queue<pair<int,int>> swan_ice;
+int swan_idx[2][2];
+int dx[]={-1,0,1,0};
+int dy[]={0,1,0,-1};
 
-ll calculation(ll x, int d)
+void water_bfs()
 {
-	if (d == 0)
-		return (x + x);
-	else if (d == 1)
-		return (x - x);
-	else if (d == 2)
-		return (x * x);
-	else if (d == 3)
-		return (x / x);
-}
-
-void solution()
-{
-	queue<pair<ll, string>> q;
-	set<ll> se;
-	q.push({s, ""});
-	se.insert(s);
-
-	while (!q.empty())
+	while(!water.empty())
 	{
-		ll x = q.front().first;
-		string str = q.front().second;
-		q.pop();
+		int x = water.front().first;
+		int y = water.front().second;
+		water.pop();
 
-		if (x == t)
+		for(int dir =0; dir<4;dir++)
 		{
-			cout << str << '\n';
-			break;
-		}
+			int nx = x+ dx[dir];
+			int ny = y +dy[dir];
 
-		for (int dir = 0; dir < 4; dir++)
-		{
-			ll nx = calculation(x, dir);
+			if(nx<0||ny<0||nx>=r||ny>=c)
+			continue;
 
-			if (nx < 1)
-				continue;
-			if (se.find(nx) != se.end()) //이미 있을 경우 continue;
-				continue;
-
-			se.insert(nx);
-			q.push({nx, str + op[dir]});
+			if(map[nx][ny]=='X')
+			{
+				map[nx][ny]='.';
+				water_ice.push({nx,ny});
+			}
 		}
 	}
 }
+bool swan_bfs()
+{
+	while(!swan.empty())
+	{
+		int x = swan.front().first;
+		int y = swan.front().second;
+		swan.pop();
 
+		for(int dir = 0; dir<4;dir++)
+		{
+			int nx = x + dx[dir];
+			int ny = y + dy[dir];
+			
+			if(nx<0||ny<0||nx>=r||ny>=c)
+			continue;
+
+			if(check[nx][ny])
+			continue;
+
+			if(map[nx][ny]=='.')
+			{swan.push({nx,ny});
+			check[nx][ny] = true;
+			}
+
+			else if(map[nx][ny]=='X')
+			{
+				swan_ice.push({nx,ny});
+				check[nx][ny] = true;
+			}
+			else if(map[nx][ny]=='L')
+			{
+				return true;
+			}
+
+		}
+	}
+	return false;
+}
+void memset_q()
+{
+	while(!swan_ice.empty())
+	swan_ice.pop();
+	while(!water_ice.empty())
+	water_ice.pop();
+}
 int main()
 {
 	ios::sync_with_stdio(false);
 	cin.tie(0);
 
-	cin >> s >> t;
+	cin >> r >> c;
+	int n = 0, day=0;
 
-	if (s == t)
-		cout << 0 << '\n';
-	else
-		solution();
+	for (int i = 0; i < r; i++)
+	{
+		for (int j = 0; j < c; j++)
+		{
+			cin >> map[i][j];
+			if (map[i][j] != 'X')
+				water.push({i, j});
+			if (map[i][j] == 'L')
+			{
+				swan_idx[n][0] = i;
+				swan_idx[n][1] = j;
+				n++;
+			}
+		}
+	}
+	swan.push({swan_idx[0][0],swan_idx[0][1]});
+	check[swan_idx[0][0]][swan_idx[0][1]] = true;
+
+	while(true)
+	{
+		//1. 만났는지 확인
+		if (swan_bfs())
+		{
+			cout<<day<<'\n';
+			break;
+		}
+		day++;
+		water_bfs();	//2.얼음 녹이기
+		water = water_ice;
+		swan = swan_ice;
+		memset_q();
+		
+	}
+
+
 	return 0;
 }
