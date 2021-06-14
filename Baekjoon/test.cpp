96bda542cee15f2c82b9b4968c8f20262fe1f55e
clappingmin <clappingmin@naver.com>
2021-06-14 16:55:16 +0900

    2021-06-14

--- Baekjoon/test.cpp
+++ Baekjoon/test.cpp
@@ -1,98 +1,45 @@
 #include <iostream>
-#include <queue>
-#include <cstring>
-#include <algorithm>
 
 using namespace std;
 
 int n;
-int map[100][100];
-bool check[100][100];
-int dx[] = {-1, 0, 1, 0};
-int dy[] = {0, 1, 0, -1};
-int dist[100][100];
-int res = 0;
 
-int create_bridge(int label)
+struct Node
 {
-	queue<pair<int, int>> q;
+	int left, right;
+};
 
-	for (int i = 0; i < n; i++)
-	{
-		for (int j = 0; j < n; j++)
-		{
-			if (map[i][j] == label)
-			{
-				q.push({i, j});
-				check[i][j] = true;
-			}
-		}
-	}
+Node a[27];
 
-	int cnt = 0;
+void preorder(int x)
+{
+	if (x == -1)
+		return;
 
-	while (!q.empty())
-	{
-		int island = q.size();
+	cout << (char)(x + 'A');
+	preorder(a[x].left);
+	preorder(a[x].right);
+}
+void inorder(int x)
+{
+	if (x == -1)
+		return;
 
-		for (int i = 0; i < island; i++)
-		{
-			int x = q.front().first;
-			int y = q.front().second;
-			q.pop();
-
-			for (int dir = 0; dir < 4; dir++)
-			{
-				int nx = x + dx[dir];
-				int ny = y + dy[dir];
-
-				if (nx < 0 || ny < 0 || nx >= n || ny >= n)
-					continue;
-
-				if (map[nx][ny] != 0 || map[nx][ny] != label)
-					return cnt;
-
-				if (!check[nx][ny] && map[nx][ny] == 0)
-				{
-					q.push({nx, ny});
-					check[nx][ny] = true;
-				}
-			}
-		}
-		cnt++;
-	}
+	inorder(a[x].left);
+	cout << (char)(x + 'A');
+	inorder(a[x].right);
 }
 
-void labeling(int i, int j, int label)
+void postorder(int x)
 {
-	queue<pair<int, int>> q;
-	check[i][j] = true;
-	q.push({i, j});
-	map[i][j] = label;
+	if (x == -1)
+		return;
 
-	while (!q.empty())
-	{
-		int x = q.front().first;
-		int y = q.front().second;
-		q.pop();
-
-		for (int dir = 0; dir < 4; dir++)
-		{
-			int nx = x + dx[dir];
-			int ny = y + dy[dir];
-
-			if (nx < 0 || ny < 0 || nx >= n || ny >= n)
-				continue;
-
-			if (map[nx][ny] == 0 || check[nx][ny])
-				continue;
-
-			map[nx][ny] = label;
-			check[nx][ny] = true;
-			q.push({nx, ny});
-		}
-	}
+	postorder(a[x].left);
+	postorder(a[x].right);
+	cout << (char)(x + 'A');
 }
+
 int main()
 {
 	ios::sync_with_stdio(false);
@@ -101,29 +48,28 @@ int main()
 	cin >> n;
 
 	for (int i = 0; i < n; i++)
-		for (int j = 0; j < n; j++)
-			cin >> map[i][j];
-
-	int label = 1;
-	for (int i = 0; i < n; i++)
 	{
-		for (int j = 0; j < n; j++)
-		{
-			if (!check[i][j] && map[i][j] == 1)
-			{
-				labeling(i, j, label);
-				label++;
-			}
-		}
-	}
+		char x, y, z;
 
-	for (int i = 1; i <= label; i++)
-	{
-		memset(check, false, sizeof(false));
-		res = min(res, create_bridge(i));
-	}
+		cin >> x >> y >> z;
 
-	cout << res << '\n';
+		x = x - 'A';
+
+		if (y == '.')
+			a[x].left = -1;
+		else
+			a[x].left = y - 'A';
+
+		if (z == '.')
+			a[x].right = -1;
+		else
+			a[x].right = z - 'A';
+	}
+	preorder(0);
+	cout<<'\n';
+	inorder(0);
+	cout<<'\n';
+	postorder(0);
 
 	return 0;
 }
