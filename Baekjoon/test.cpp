3a908a1bb37ab810223dacc5afa659971e43c296
clappingmin <clappingmin@naver.com>
2021-06-15 23:04:34 +0900

    2021-06-15

--- Baekjoon/test.cpp
+++ Baekjoon/test.cpp
@@ -1,75 +1,74 @@
 #include <iostream>
-#include <string>
-#include <vector>
-#include <algorithm>
 
 using namespace std;
 
-int n;
-vector<char> letters;
-char alpha[256];
-
-int calc(vector<string> &a, vector<int> &d)
+struct Node
 {
-	int m = letters.size();
-	int sum = 0;
+	int left, right;
+};
 
-	for (int i = 0; i < m; i++)
-	{
-		alpha[letters[i]] = d[i];
-	}
+int n;
+Node a[27];
 
-	for (string s : a)
-	{
-		int now = 0;
-		for (char x : s)
-		{
-			now = now * 10 + alpha[x];
-		}
-		sum += now;
-	}
-	return sum;
+void preorder(int x)
+{
+	if(x==-1)
+	return;
+
+	cout<<(char)(x+'A');
+	preorder(a[x].left);
+	preorder(a[x].right);
 }
+void inorder(int x)
+{
+	if(x==-1)
+	return;
 
+	inorder(a[x].left);
+	cout<<(char)(x+'A');
+	inorder(a[x].right);
+}
+void postorder(int x)
+{
+	if(x==-1)
+	return;
+
+	postorder(a[x].left);
+	postorder(a[x].right);
+	cout<<(char)(x+'A');
+
+}
 int main()
 {
 	ios::sync_with_stdio(false);
 	cin.tie(0);
 
 	cin >> n;
-	vector<string> a(n); //단어 저장
 
 	for (int i = 0; i < n; i++)
 	{
-		cin >> a[i];
+		char x, y, z;
 
-		for (char x : a[i])
-			letters.push_back(x);
+		cin >> x >> y >> z;
+
+		x = x - 'A';
+
+		if (y == '.')
+			a[x].left = -1;
+		else
+			a[x].left = y - 'A';
+
+		if (z == '.')
+			a[x].right = -1;
+		else
+			a[x].right = z - 'A';
 	}
 
-	sort(letters.begin(), letters.end());
-	letters.erase(unique(letters.begin(), letters.end()), letters.end()); //중복제거
-
-	int m = letters.size();
-
-	vector<int> d;
-
-	for (int i = 0; i < m; i++)
-	{
-		d.push_back(9 - i);
-	}
-
-	int res = 0;
-	
-	do
-	{
-		int now = calc(a, d);
-
-		res = max(res, now);
-
-	} while (next_permutation(d.begin(), d.end()));
-
-	cout << res << '\n';
+	preorder(0);
+	cout<<'\n';
+	inorder(0);
+	cout<<'\n';
+	postorder(0);
 
 	return 0;
 }
