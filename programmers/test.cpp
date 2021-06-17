cf0d1262306842dfde80b714d5cb21dcfa0a415d
clappingmin <clappingmin@naver.com>
2021-06-17 22:42:47 +0900

    2021-06-17

--- programmers/test.cpp
+++ programmers/test.cpp
@@ -1,86 +1,40 @@
-#include <string>
-#include <vector>
-#include <queue>
 #include <iostream>
-#include <cstring>
+#include <algorithm>
+#include <climits>
 
 using namespace std;
 
-int dx[] = {-1, 0, 1, 0};
-int dy[] = {0, 1, 0, -1};
+int n;
+int t[16], p[16]; //상담일, 금액
+int res = INT_MIN;
 
-vector<int> solution(vector<vector<string>> places)
+void go(int day, int sum)
 {
-    vector<int> answer;
-    int t = 5;
-    int check[5][5];
-
-    while (t--)
+    if (day == n + 1)
     {
-        for (int i = 0; i < 5; i++)
-        {
-            for (int j = 0; j < 5; j++)
-            {
-                if (places[t][i][j] == 'P')
-                    all_q.push({i, j});
-            }
-        }
-
-        bool check_final = false;
-
-        while (!all_q.empty())
-        {
-            bool is_ok = true;
-            memset(check, -1, sizeof(check));
-            queue<pair<int, int>> q;
-            int i = all_q.front().first;
-            int j = all_q.front().second;
-
-            all_q.pop();
-            q.push({i, j});
-
-            check[i][j] = 0;
-
-            while (!q.empty())
-            {
-                int x = q.front().first;
-                int y = q.front().second;
-                q.pop();
-
-                if (is_ok == false)
-                    break;
-
-                for (int dir = 0; dir < 4; dir++)
-                {
-                    int nx = x + dx[dir];
-                    int ny = y + dy[dir];
-
-                    if (nx < 0 || ny < 0 || nx >= 5 || ny >= 5)
-                        continue;
-                    if (places[t][nx][ny] == 'X' || check[nx][ny] != -1)
-                        continue;
-
-                    check[nx][ny] = check[x][y] + 1;
-                    q.push({nx, ny});
-
-                    if (places[t][nx][ny] == 'P' && check[nx][ny] <= 2)
-                    {
-                        is_ok = false;
-                        break;
-                    }
-                }
-            }
-
-            if (is_ok == false)
-            {
-                answer.push_back(0);
-                check_final = true;
-                break;
-            }
-        }
-        if (check_final == false)
-            answer.push_back(1);
+        res = max(res, sum);
+        return;
     }
+    if (day > n + 1)
+        return;
 
-    return answer;
+    go(day + t[day], sum + p[day]);
+    go(day + 1, sum);
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    cin >> n;
+
+    for (int i = 1; i <= n; i++)
+        cin >> t[i] >> p[i];
+
+    go(1, 0);
+
+    cout<<res<<'\n';
+
+    return 0;
 }
