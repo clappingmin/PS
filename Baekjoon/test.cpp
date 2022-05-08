5673d3adb5adf7934a1078dd2cade0af685ae37c
Sumin Park <clappingmin@gmail.com>
2022-05-08 21:55:39 +0900

    2022-05-08

--- Baekjoon/test.cpp
+++ Baekjoon/test.cpp
@@ -1,17 +1,69 @@
 #include <iostream>
+#include <string>
+#include <stack>
 
 using namespace std;
 
-int w,h;
-char map
-
 int main()
 {
-    ios::sync_with_stdio(false);
-    cin.tie(0);
+  ios::sync_with_stdio(false);
+  cin.tie(0);
 
-    cin>>w>>h;
+  int testcase = 0;
 
+  cin >> testcase;
+  stack<int> s;
 
-    return 0;
+  while (testcase--)
+  {
+    string cmd = "";
+    int num = 0;
+    cin >> cmd;
+
+    if (cmd == "push")
+    {
+      cin >> num;
+      s.push(num);
+    }
+    else if (cmd == "pop")
+    {
+      if (s.empty())
+      {
+        cout << -1 << '\n';
+      }
+      else
+      {
+        cout << s.top() << '\n';
+        s.pop();
+      }
+    }
+    else if (cmd == "size")
+    {
+      cout << s.size() << '\n';
+    }
+    else if (cmd == "empty")
+    {
+      if (s.empty())
+      {
+        cout << 1 << '\n';
+      }
+      else
+      {
+        cout << 0 << '\n';
+      }
+    }
+    else if (cmd == "top")
+    {
+      if (s.empty())
+      {
+        cout << -1 << '\n';
+      }
+      else
+      {
+        cout << s.top() << '\n';
+      }
+    }
+  }
+
+  return 0;
 }
--- README.md
+++ README.md
@@ -1,8 +1,14 @@
-### Langauge
-* C++   
-* * *
-### Online Judge
-* [Baekjoon](https://www.acmicpc.net/)   
-* [SW Expert Academy](https://swexpertacademy.com/main/main.do)   
-* [CodeUp](https://codeup.kr/index.php)   
-* [programmers](https://programmers.co.kr/)
+# Online Judge
+
+- [Baekjoon](https://www.acmicpc.net/)
+- [SW Expert Academy](https://swexpertacademy.com/main/main.do)
+- [CodeUp](https://codeup.kr/index.php)
+- [programmers](https://programmers.co.kr/)
+
+---
+
+## 자료구조
+
+### 백준
+
+- 10828: 스택
